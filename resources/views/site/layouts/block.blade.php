c154acd443b41db934237dd499c7b99aeddf943b
Kevin Lim <zzkevinlim@gmail.com>
2023-11-28 13:46:46 +1300

    fix preview

--- resources/views/site/layouts/block.blade.php
+++ resources/views/site/layouts/block.blade.php
@@ -1,11 +1,15 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <title>#madewithtwill website</title>
+    @vite('resources/css/app.css')
 </head>
+
 <body>
-<div>
-    @yield('content')
-</div>
+    <div>
+        @yield('content')
+    </div>
 </body>
+
 </html>
