020a232fe71e9a3b93ae6ce36afc44d08448b43e
Kevin Lim <zzkevinlim@gmail.com>
2023-11-28 13:40:19 +1300

    create block

--- config/twill.php
+++ config/twill.php
@@ -10,5 +10,24 @@ return [
 				]
 			]
 		]
+	],
+	'block_editor' => [
+		'use_twill_blocks' => [],
+		'crops' => [
+			'highlight' => [
+				'desktop' => [
+					[
+						'name' => 'desktop',
+						'ratio' => 16 / 9,
+					],
+				],
+				'mobile' => [
+					[
+						'name' => 'mobile',
+						'ratio' => 1,
+					],
+				],
+			],
+		],
 	]
 ];
---
+++ resources/views/twill/blocks/image.blade.php
@@ -0,0 +1,5 @@
+@twillBlockTitle('Image')
+@twillBlockIcon('text')
+@twillBlockGroup('app')
+
+<x-twill::medias name="highlight" label="Highlight" />
