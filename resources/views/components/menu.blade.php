28d1ab03cda1493726d5455ffbdec7f53d96b86b
Kevin Lim <zzkevinlim@gmail.com>
2023-12-06 14:05:09 +1300

    implement localisation amendments #2

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -12,7 +12,7 @@
         @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
         <li>
             <a rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
-                {{ strtoupper($localeCode) }}
+                {{ $properties['native'] }}
             </a>
         </li>
         @endforeach
