545cd7b87ffcd0dfa76f59fe03176329265831f7
Kevin Lim <zzkevinlim@gmail.com>
2023-12-12 12:11:28 +1300

    project gallery is now using a swiper

---
+++ resources/js/projectswiper.js
@@ -0,0 +1,27 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+	direction: 'horizontal',
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	// And if we need scrollbar
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+});
--- resources/views/site/project.blade.php
+++ resources/views/site/project.blade.php
@@ -10,12 +10,30 @@
     @include('site.header')
     <div class="container mx-auto">
         {!! $item->renderBlocks() !!}
-        <div class="space-y-8">
-            @foreach($item->images('cover') as $projectImage)
-            <img src="{{ $projectImage }}" alt="">
-            @endforeach
+
+        <!-- Slider main container -->
+        <div class="swiper">
+            <!-- Additional required wrapper -->
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                @foreach($item->images('cover') as $projectImage)
+                <div class="swiper-slide">
+                    <img src="{{ $projectImage }}" alt="">
+                </div>
+                @endforeach
+            </div>
+            <!-- If we need pagination -->
+            <div class="swiper-pagination"></div>
+
+            <!-- If we need navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+
+            <!-- If we need scrollbar -->
+            <div class="swiper-scrollbar"></div>
         </div>
     </div>
+    @vite(['resources/js/projectswiper.js'])
 </body>
 
 </html>
